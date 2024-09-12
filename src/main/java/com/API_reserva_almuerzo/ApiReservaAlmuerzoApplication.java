744addc31c7a7af6e3453f50bb7d6c68a993ace8
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2024-09-12 11:15:02 -0300

    Initial: creacion API reserva de almuerzo

---
+++ src/main/java/com/API_reserva_almuerzo/ApiReservaAlmuerzoApplication.java
@@ -0,0 +1,13 @@
+package com.API_reserva_almuerzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiReservaAlmuerzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiReservaAlmuerzoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/API_reserva_almuerzo/ApiReservaAlmuerzoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API_reserva_almuerzo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiReservaAlmuerzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
